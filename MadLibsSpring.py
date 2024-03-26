08a63a14aa96a54453cc1f3a66c0e4e8e44401bc
Cherissonne28 <163193097+Cherissonne28@users.noreply.github.com>
2024-03-26 21:44:07 +0000

    tidied up the code a bit

--- MadLibsSpring.py
+++ MadLibsSpring.py
@@ -1,7 +1,14 @@
+#A programme to facilitate a shared game of MadLibs, in a group of 7. 
+#The general theme is Spring. 
+#It makes use of user input and string manipulation. 
+
+#User inputs for Paragraph 1: 
 Adjective = input("Please enter an adjective. ")
 Noun_1 = input("Please enter a generic noun. ")
 Exclamation = input("Please enter an exclamation, such as 'At last!'. ")
 Past_participle_have1 = input("Please enter a past participle that goes with the verb to have. ")
+
+#User Inputs for Paragraph 2: 
 Proper_noun1 = input("Please enter a proper noun, such as a name. ")
 Capitalized_Noun = input("Please enter a noun describing a type of thing, with the initial letter capitalized. ")
 Pronoun_poss = input("Please enter a possessive pronoun. ")
@@ -9,10 +16,17 @@ verb_1 = input("Please enter a past tense verb. ")
 noun_4 = input("Please enter a generic noun. ")
 plural_noun1 = input("Please enter a plural noun. ")
 present_participle = input("Please enter a present participle, such as 'carrying'. ")
+
+#User Inputs for paragraph 3: 
 Past_participle_be1 = input("Please enter a past participle that goes with the verb to be. ")
 plural_noun2 = input("Please enter a plural noun. ")
 verb_3 = input("Please enter a past tense verb. ")
 
+
 print(f"The soil combination was just {Adjective}. The {Noun_1} was balmy, without being too dry. {Exclamation}! Spring had {Past_participle_have1}. ")
+
+
 print(f"{Proper_noun1} the {Capitalized_Noun} gathered {Pronoun_poss} resources, and {verb_1} a {noun_4} above the surface. All at once {Proper_noun1} was aware of many other {plural_noun1} also {present_participle} through the surfaces. ")
+
+
 print(f"Over the next few days {Proper_noun1} became {Past_participle_be1} with many other {plural_noun2}: insects, birds, animals, lichens and fungi in the nearby area. The sun shone and everything rejoiced. ")
